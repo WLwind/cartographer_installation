10b2de7c4f2697cdb40e882d8466533ed0d960cb
WLwind <1121391435@qq.com>
2020-07-04 17:33:55 +0800

    fix the bug of sudo command

--- install_without_rosdep_init.sh
+++ install_without_rosdep_init.sh
@@ -3,7 +3,7 @@ set -o errexit
 VERSION="v3.4.1"
 sudo apt update
 sudo apt install -y python-wstool python-rosdep ninja-build
-sudo echo "199.232.28.133 raw.githubusercontent.com" >> /etc/hosts
+sudo sh -c "echo '199.232.28.133 raw.githubusercontent.com' >> /etc/hosts"
 rosdep init
 rosdep update
 mkdir -p ~/cartographer_ws/src
